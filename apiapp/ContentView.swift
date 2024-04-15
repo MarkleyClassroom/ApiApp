f823f45ddb6d0d65b4bc4bd005529b9687936eaf
Kirsten Markley <markleyk@otc.edu>
2024-04-15 05:25:00 -0500

    Stage 1

--- apiapp/ContentView.swift
+++ apiapp/ContentView.swift
@@ -6,16 +6,88 @@
 //
 
 import SwiftUI
+// API url  https://api.github.com/search/users?q=greg
+
+// Both structs use Codable so that we can passs them to the JSON Decord to decode the JSON response string back into the structs
+
+// Structs will contain the information returned from the JSON
+// NOTE that the variable names have to be exactly like in the JSON file
+
+//indivisual User from the json
+struct User: Codable {
+    public var login: String
+    public var url: String
+    public var avatar_url:String
+    public var html_url: String
+}
+// the items array from the JSON
+struct Result: Codable {
+    var items:[User]
+}
 
 struct ContentView: View {
+    @State var users:[User] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            if users.count == 0{
+                //display a progress spinning wheel if no data has been pulled yet
+                VStack{
+                    ProgressView().padding()
+                    Text("Fetching Users...")
+                        .foregroundStyle(Color.purple)
+                        .onAppear{
+                            getUsers()
+                        }
+                }
+            } else {
+                // bind the list to the User array
+                List(users, id:\.login) {user in
+                    // links to their github profile using Safari
+                    Link(destination:URL(string:user.html_url)!){
+                        
+                        
+                        // diplay the image
+                        HStack(alignment:.top){
+                            AsyncImage(url:URL(string: user.avatar_url)){ response in
+                                switch response {
+                                case .success(let image):
+                                    image.resizable()
+                                        .frame(width:50, height: 50)
+                                default:
+                                    Image(systemName:"nosign")
+                                }
+                            }
+                        }
+                        
+                        // display the user info
+                        VStack(alignment: .leading){
+                            Text(user.login)
+                            Text("\(user.url)")
+                                .font(.system(size:11))
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    
+    // fetches the Users from the github api
+    
+    func getUsers(){
+        if let apiURL = URL(string:"https://api.github.com/search/users?q=greg"){
+            var request = URLRequest(url:apiURL)
+            request.httpMethod = "GET"
+            URLSession.shared.dataTask(with: request){
+                data, response,error in
+                if let userData = data {
+                    if let usersFromAPI = try? JSONDecoder().decode(Result.self, from: userData){
+                        users = usersFromAPI.items
+                        print(users)
+                    }
+                }
+            }.resume()
+        }
     }
 }
 
