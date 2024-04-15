e20a7cee78f046c4f7c4c2d7349e804a1b651529
Kirsten Markley <markleyk@otc.edu>
2024-04-15 04:48:47 -0500

    Initial Commit

---
+++ apiapp/apiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiappApp.swift
+//  apiapp
+//
+//  Created by KIRSTEN Markley on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct apiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
